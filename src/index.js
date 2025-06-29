5c8e6a12b3ac094eb59750be943e6a08b38c1a0c
Ronin87 <anandranjith8792@gmail.com>
2025-06-29 23:25:39 +0530

    Ensured index.js imports RootApp correctly after App.js export fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Assuming you have an index.css
-import RootApp from './App'; // <--- CHANGED THIS LINE to import RootApp
+import RootApp from './App'; // <--- This line should be 'RootApp'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RootApp /> {/* <--- CHANGED THIS LINE to render RootApp */}
+    <RootApp /> {/* <--- This line should render 'RootApp' */}
   </React.StrictMode>
 );
 
