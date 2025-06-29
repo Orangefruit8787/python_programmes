97c698641c1f729edbd9972da72dc3908c22f26d
Ronin87 <anandranjith8792@gmail.com>
2025-06-29 23:07:34 +0530

    Fix:Import RootApp correctly included in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Assuming you have an index.css
-import App from './App'; // Make sure this path correctly points to your App.js
-import reportWebVitals from './reportWebVitals'; // Import the web-vitals reporter
+import RootApp from './App'; // <--- CHANGED THIS LINE to import RootApp
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RootApp /> {/* <--- CHANGED THIS LINE to render RootApp */}
   </React.StrictMode>
 );
+
 reportWebVitals();
+
